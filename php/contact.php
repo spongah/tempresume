59914fd01405e787ef607f4bf91461692f08cbd7
spongah <rocksolid801@gmail.com>
2017-07-17 14:44:53 -0700

    update php contact form to sanitize data

--- php/contact.php
+++ php/contact.php
@@ -1,11 +1,19 @@
-<?php $name = $_POST['name'];
-$email = $_POST['email'];
-$number = $_POST['number'];
-$message = $_POST['msg'];
+<?php
+function checkInput($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data
+}
+
+$name = checkInput($_POST['name']);
+$email = checkInput($_POST['email']);
+$number = checkInput($_POST['number']);
+$message = checkInput($_POST['msg']);
 $formcontent="From: $name\nNumber: $number\nMessage: $message";
 $recipient = "ericeschelbach@gmail.com";
 $subject = "Contact Form from EricEschelbach.com";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-header("Location: ../index.html");
+header("Location: ../index.ejs");
 ?>
